ef176ded9982bcdb6dc29ba0c7dd4dd07232ae4d
Dan Mace <dmacebeta@gmail.com>
2019-03-11 14:25:56 -0700

    fix folder

--- credentials/client.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyBa8xdV-zoVl11uLIGom06bB5LC6TbZOOE",
-  authDomain: "guidery.firebaseapp.com",
-  databaseURL: "https://guidery.firebaseio.com",
-  projectId: "guidery",
-  storageBucket: "guidery.appspot.com",
-  messagingSenderId: "600882070936"
-};
